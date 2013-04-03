95f5bcc0814559822fb17437634b37d09261e7ec
dirtybit <sertacolgunsoylu@gmail.com>
2013-04-03 13:27:11 +0300

    Tracker:  - Marker mapping added  - Verbose output added  - Failure frame logging imported

--- tracker/src/tracker.cpp
+++ tracker/src/tracker.cpp
@@ -16,6 +16,7 @@ using namespace cv;
 using namespace aruco;
 
 #define PORT 9090
+#define NUM_OF_MARKERS 209
 
 sem_t sem;
 pthread_mutex_t mutex;
@@ -24,6 +25,88 @@ int width=320;
 int height=240;
 
 char pos_data[200];
+int found;
+
+struct coord {
+	float x;
+	float y;
+} markerCoords[NUM_OF_MARKERS];
+
+int verbose = 0;
+int failedFrameNumber = 0;
+int saveFailedFrames = 0;
+char* savedFrame = new char[25];
+char* outputPath = new char[200];
+
+void initMarkerCoordinates()
+{
+	for (int i = 0; i < NUM_OF_MARKERS; i++) {
+		if (i < 60) {
+			markerCoords[i].x = i  % 6;
+			markerCoords[i].y = i / 6;
+		} else if (i < 65) {
+			markerCoords[i].x = (i - 60.0) + 0.5;
+			markerCoords[i].y = 0.0;
+		} else if (i < 76) {
+			markerCoords[i].x = (i - 65.0) * 0.5;
+			markerCoords[i].y = 0.5;
+		} else if (i < 81) {
+			markerCoords[i].x = (i - 76.0) + 0.5;
+			markerCoords[i].y = 1.0;
+		} else if (i < 92) {
+			markerCoords[i].x = (i - 81.0) * 0.5;
+			markerCoords[i].y = 1.5;
+		} else if (i < 97) {
+			markerCoords[i].x = (i - 92.0) + 0.5;
+			markerCoords[i].y = 2.0;
+		} else if (i < 108) {
+			markerCoords[i].x = (i - 97.0) * 0.5;
+			markerCoords[i].y = 2.5;
+		} else if (i < 113) {
+			markerCoords[i].x = (i - 108.0) + 0.5;
+			markerCoords[i].y = 3.0;
+		} else if (i < 124) {
+			markerCoords[i].x = (i - 113.0) * 0.5;
+			markerCoords[i].y = 3.5;
+		} else if (i < 129) {
+			markerCoords[i].x = (i - 124.0) + 0.5;
+			markerCoords[i].y = 4.0;
+		} else if (i < 140) {
+			markerCoords[i].x = (i - 129.0) * 0.5;
+			markerCoords[i].y = 4.5;
+		} else if (i < 145) {
+			markerCoords[i].x = (i - 140.0) + 0.5;
+			markerCoords[i].y = 5.0;
+		} else if (i < 156) {
+			markerCoords[i].x = (i - 145.0) * 0.5;
+			markerCoords[i].y = 5.5;
+		} else if (i < 161) {
+			markerCoords[i].x = (i - 156.0) + 0.5;
+			markerCoords[i].y = 6.0;
+		} else if (i < 172) {
+			markerCoords[i].x = (i - 161.0) * 0.5;
+			markerCoords[i].y = 6.5;
+		} else if (i < 177) {
+			markerCoords[i].x = (i - 172.0) + 0.5;
+			markerCoords[i].y = 7.0;
+		} else if (i < 188) {
+			markerCoords[i].x = (i - 177.0) * 0.5;
+			markerCoords[i].y = 7.5;
+		} else if (i < 193) {
+			markerCoords[i].x = (i - 188.0) + 0.5;
+			markerCoords[i].y = 8.0;
+		} else if (i < 204) {
+			markerCoords[i].x = (i - 193.0) * 0.5;
+			markerCoords[i].y = 8.5;
+		} else {
+			markerCoords[i].x = (i - 204.0) + 0.5;
+			markerCoords[i].y = 9.0;
+
+		}
+	}
+}
+
+
 
 cv::Mat rmat(cv::Mat u, cv::Mat v, cv::Mat w) {
 	cv::Mat m = cv::Mat::eye(4, 4, CV_32F);
@@ -44,14 +127,13 @@ cv::Mat tmat(cv::Mat t) {
 	return m;
 }
 
-cv::Mat localize(MarkerDetector &markerDetector, cv::Mat &frame, vector<Marker> &markers, CameraParameters cameraParameters, float markerSize)
+int localize(MarkerDetector &markerDetector, cv::Mat &frame, vector<Marker> &markers, CameraParameters cameraParameters, float markerSize, cv::Mat &pos3D)
 {
 	markerDetector.detect(frame, markers, cameraParameters, markerSize);
 	    
 	float maxArea = 0;
 	float markerArea = 0;
 	Marker *chosen = NULL;
-	cv::Mat pos3D = cv::Mat::zeros(4, 1, CV_32F);
 
 	for (unsigned int i=0;i<markers.size();i++) {
 		markerArea = markers[i].getArea();
@@ -89,12 +171,19 @@ cv::Mat localize(MarkerDetector &markerDetector, cv::Mat &frame, vector<Marker> 
 		cv::Mat T = tmat(coord);
 		cv::Mat M = R*T;
 		coord_ = M*coord_;
-		pos3D.at<float>(0, 0) = -coord_.at<float>(2, 0);
-		pos3D.at<float>(1, 0) = -coord_.at<float>(0, 0);
-		pos3D.at<float>(2, 0) = coord_.at<float>(1, 0);
-	}
 
-	return pos3D;
+		float x = markerCoords[chosen->id].x;
+		float y = markerCoords[chosen->id].y;
+		float z = coord_.at<float>(1, 0);
+		pos3D.at<float>(0, 0) = x - coord_.at<float>(2, 0);
+		pos3D.at<float>(1, 0) = y - coord_.at<float>(0, 0);
+		pos3D.at<float>(2, 0) = z;
+		pos3D.at<float>(3, 0) = 1;
+		
+		return 1;
+	}
+	else
+		return 0;
 }
 
 void* run_server(void *args)
@@ -139,7 +228,8 @@ void* run_server(void *args)
 	while(1) {
 		sem_wait(&sem);
 		pthread_mutex_lock(&mutex);
-		send(conn, pos_data, strlen(pos_data), 0);
+		if (found)
+			send(conn, pos_data, strlen(pos_data), 0);
 		pthread_mutex_unlock(&mutex);
 	}
 }
@@ -158,7 +248,7 @@ int main(int argc,char **argv)
 	pair<double,double> avgTime(0,0) ;//determines the average time required for detection
 	float thresParam1, thresParam2;		
 	int index=0;
-	Mat pos3D;
+	Mat pos3D = cv::Mat::zeros(4, 1, CV_32F);
 	double tick;
 
 	// Initialize sync 	
@@ -168,6 +258,8 @@ int main(int argc,char **argv)
 
 	pthread_mutex_init(&mutex, NULL);
 	sem_init(&sem, 0, 0);
+
+	initMarkerCoordinates();
 
 
 	pthread_create(&child, &attr, run_server, NULL);
@@ -186,14 +278,24 @@ int main(int argc,char **argv)
 			width=atoi(argv[2]);
 		if (argc > 3)
 			height=atoi(argv[3]);
-
 		if (argc > 4)
 			intrinsicFile=argv[4];
 		if (argc > 5)
 			markerSize=atof(argv[5]);
+		if (argc > 6)
+			verbose=atoi(argv[6]);
+		if (argc > 7)
+			saveFailedFrames=atoi(argv[7]);
+		if (argc > 8)
+			outputPath=argv[8];
 
-		if (argc==5)
+		if (argc == 5)
 			cerr<<"NOTE: You need makersize to see 3d info!!!!"<<endl;
+
+		if (argc == 8) {
+			cerr<<"NOTE: You have to specify output path!!!!"<<endl;
+			exit(0);
+		}
 
 		//read from camera or from  file
 		if (inputSource=="live") {
@@ -232,8 +334,8 @@ int main(int argc,char **argv)
 		markerDetector.setThresholdParams(thresParam1, thresParam2);
 
 		tick = (double)getTickCount(); // Time of detection
-            
-		int k=0;
+		
+		int res;
 		while (capture.grab())
 		{
 			capture.retrieve(inputImage);
@@ -244,12 +346,29 @@ int main(int argc,char **argv)
 			index++; //number of images captured
 			tick = (double)getTickCount();
 	    
-			pos3D = localize(markerDetector, inputImage, markers, cameraParameters, markerSize);
+			res = localize(markerDetector, inputImage, markers, cameraParameters, markerSize, pos3D);
 
-			float x = pos3D.at<float>(0, 0); //chosen->id % 6;
-			float y = pos3D.at<float>(1, 0); //chosen->id / 6;
+			if(verbose) {
+				if (res == 0) // no marker in the current frame
+					failedFrameNumber++;
+
+				cerr << "No markers in " << failedFrameNumber << " frames out of " << index << endl;
+			}
+
+			if(saveFailedFrames){
+				if (res == 0) {
+					sprintf(savedFrame, "%sframe_%d.png", outputPath, index);
+					imwrite(savedFrame, inputImage);
+					cout << "Frame " << index << " saved to: " << savedFrame << endl;
+				}
+
+			}
+
+			float x = pos3D.at<float>(0, 0);
+			float y = pos3D.at<float>(1, 0);
 			float z = pos3D.at<float>(2, 0);
 			pthread_mutex_lock(&mutex);
+			found = res;
 			sprintf(pos_data, "%.6f, %.6f, %.6f, %.6f", x, y, z, 1000*avgTime.first);
 			cout << "Fm = " << pos_data << endl;
 			pthread_mutex_unlock(&mutex);			
